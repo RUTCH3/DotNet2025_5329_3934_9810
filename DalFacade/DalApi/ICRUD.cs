9adc007de260d5301a32fb32fffcff3fd3d9810b
Racheli2 <r0556732756@gmail.com>
2024-12-05 00:29:19 +0200

    שיניתי לפי סעיף 3 בשכפול של שלב 2 ב'

--- DalFacade/DalApi/ICRUD.cs
+++ DalFacade/DalApi/ICRUD.cs
@@ -5,13 +5,14 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
+
 namespace DalApi
 {
     public interface ICRUD<T>
     {
         int Create(T item); //Creates new entity object in DAL
         T? Read(int id); //Reads entity object by its ID 
-        List<T> ReadAll(); //stage 1 only, Reads all entity objects
+        List<T?> ReadAll(Func<T,bool>? filter = null); //stage 2
         void Update(T item); //Updates entity object
         void Delete(int id); //Deletes an object by its Id 
     }
